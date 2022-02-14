6057bc5c6c19e473a186db2ab79339f927b83e3f
GeThem <99418179+GeThem@users.noreply.github.com>
2022-02-14 17:03:16 +0300

    Empty zone opening almost done

--- game.py
+++ game.py
@@ -15,7 +15,7 @@ pygame.display.set_caption('Minesweeper')
 behind_tiles_color = (235, 235, 235)
 background_color = (191, 196, 199)
 # tiles configuration
-tile_size = 40
+tile_size = 30
 tile_color = (112, 146, 190)
 active_tile_color = (142, 176, 220)
 open_tile_color = (191, 204, 215)
@@ -58,30 +58,51 @@ was_pressed = False
 was_pressed_counter = 0
 
 
-def clear(x, y, cur_tile, status):
+def clear(x, y, status):
     '''Returns False if there's a mine'''
     if 1 <= status <= 4:
+        cur_elem = MS.matrix[y][x]
         pygame.draw.rect(screen, open_tile_color,
                          (1 + boarder + x * (tile_size + 1), 1 + top_info_size + y * (tile_size + 1),
                           tile_size, tile_size))
-        if cur_tile > -1 and 1 <= status <= 2:
-            number = pygame.font.SysFont('miriam', tile_size, 1).render(str(cur_tile), 1, numbers_colors[cur_tile])
+        if cur_elem > -1 and 1 <= status <= 2:
+            number = pygame.font.SysFont('miriam', tile_size, 1).render(str(cur_elem), 1, numbers_colors[cur_elem])
             n_size = number.get_size()
             screen.blit(number, ((tile_size - n_size[0]) / 2 + 2 + boarder + x * (tile_size + 1), (tile_size - n_size[1]) / 2 + 2 + top_info_size + y * (tile_size + 1)))
         elif status == 3:
-            if cur_tile == -1:
+            if cur_elem == -1:
                 screen.blit(mine_img, (1 + boarder + x * (tile_size + 1), 1 + top_info_size + y * (tile_size + 1)))
             else:
-                number = pygame.font.SysFont('miriam', tile_size, 1).render(str(cur_tile), 1, numbers_colors[cur_tile])
+                number = pygame.font.SysFont('miriam', tile_size, 1).render(str(cur_elem), 1, numbers_colors[cur_elem])
                 n_size = number.get_size()
                 screen.blit(number, ((tile_size - n_size[0]) / 2 + 2 + boarder + x * (tile_size + 1), (tile_size - n_size[1]) / 2 + 2 + top_info_size + y * (tile_size + 1)))
         elif status == 4:
-            if cur_tile == -1:
+            if cur_elem == -1:
                 screen.blit(mine_img, (1 + boarder + x * (tile_size + 1), 1 + top_info_size + y * (tile_size + 1)))
                 screen.blit(flag_img, (1 + boarder + x * (tile_size + 1), 1 + top_info_size + y * (tile_size + 1)))
             else:
                 screen.blit(no_mine_img, (1 + boarder + x * (tile_size + 1), 1 + top_info_size + y * (tile_size + 1)))
         tiles[y][x] = 0
+
+
+def find_move(x, y):
+    if y < MS.rows - 1 and MS.matrix[y+1][x] == 0 and tiles[y+1][x] == 1:
+        return x, y+1
+    if x > 0 and MS.matrix[y][x-1] == 0 and tiles[y][x-1] == 1:
+        return x-1, y
+    if x > 0 and y < MS.rows - 1 and  MS.matrix[y+1][x-1] == 0 and tiles[y+1][x-1] == 1:
+        return x-1, y+1
+
+    if y > 0 and MS.matrix[y-1][x] == 0 and tiles[y-1][x] == 1:
+        return x, y - 1
+    if x < MS.columns - 1 and y > 0 and  MS.matrix[y-1][x+1] == 0 and tiles[y-1][x+1] == 1:
+        return x+1, y-1
+
+    if x < MS.columns - 1 and MS.matrix[y][x + 1] == 0 and tiles[y][x + 1] == 1:
+        return x + 1, y
+    if x < MS.columns - 1 and y < MS.rows - 1 and MS.matrix[y+1][x+1] == 0 and tiles[y+1][x+1] == 1:
+        return x+1, y+1
+    return -1, -1
 
 
 # draw closed tiles
@@ -153,30 +174,34 @@ while 1:
     # if right button was released on closed tile, draw numbers
     if isinstance(cur_tile, tuple):
         if cur_tile[3] == 0:
-            clear(cur_tile[0], cur_tile[1], 0, 1)
+            clear(cur_tile[0], cur_tile[1], 1)
             sum_of_closed_tiles -= 1
-            for choicey in (cur_tile[1], MS.rows, 1), (MS.rows - 2, -1, -1), (1, MS.rows, 1):
-                for y2 in range(*choicey):
-                    row_was_cleaned = 0
-                    for choicex in (cur_tile[0] + 1, MS.columns, 1), (MS.columns-2, -1, -1), (1, MS.columns, 1):
-                        for x2 in range(*choicex):
-                            if MS.matrix[y2][x2-choicex[2]] == 0 or MS.matrix[y2 - choicey[2]][x2] == 0:
-                                if clear(x2, y2, MS.matrix[y2][x2], tiles[y2][x2]):
-                                    sum_of_closed_tiles -= 1
-                    if not row_was_cleaned:
+
+            x2, y2 = cur_tile[:2]
+            storage = [(x2, y2)]
+            while 0 <= x2 <= MS.columns - 1 and 0 <= y2 <= MS.rows - 1:
+                x2, y2 = find_move(x2, y2)
+                if x2 == -1:
+                    if storage:
+                        x2, y2 = storage.pop()
+                        continue
+                    else:
                         break
+                storage.append((x2, y2))
+                clear(x2, y2, 1)
+                sum_of_closed_tiles -= 1
         # lose condition
         elif cur_tile[3] == -1:
             game_is_going = 0
-            clear(cur_tile[0], cur_tile[1], -1, 3)
+            clear(cur_tile[0], cur_tile[1], 3)
             for y, row in enumerate(tiles):
                 for x, status in enumerate(row):
                     if 1 <= status <= 2:
-                        clear(x, y, MS.matrix[y][x], 3)
+                        clear(x, y, 3)
                     if 5 <= status <= 6:
-                        clear(x, y, MS.matrix[y][x], 4)
+                        clear(x, y, 4)
         else:
-            clear(cur_tile[0], cur_tile[1], cur_tile[3], 1)
+            clear(cur_tile[0], cur_tile[1], 1)
             sum_of_closed_tiles -= 1
 
     p_tile = [mouse_pos_ind[0], mouse_pos_ind[1], tile_status]
@@ -185,7 +210,7 @@ while 1:
     if MS.minecount != counter_prev or was_pressed_counter == 0:
         counter_prev = MS.minecount
         pygame.draw.rect(screen, tile_color,
-                         (WINDOW_SIZE[0] / 2 - 40, top_info_size / 2 - 24, 80, 46))
+                         (WINDOW_SIZE[0] / 2 - 30, top_info_size / 2 - 24, 60, 46))
         mine_count = pygame.font.SysFont('arial', 37, 1).render(str(counter_prev), 1, (150, 2, 2))
         mine_count_size = mine_count.get_size()
         screen.blit(mine_count, (WINDOW_SIZE[0] / 2 - mine_count_size[0] / 2, top_info_size / 2 - mine_count_size[1] / 2))
@@ -195,7 +220,7 @@ while 1:
         if time_start:
             time_now = time() - time_start
         pygame.draw.rect(screen, tile_color,
-                         (WINDOW_SIZE[0] * 4 / 5 - 45, top_info_size / 2 - 18, 90, 36))
+                         (WINDOW_SIZE[0] * 4 / 5 - 36, top_info_size / 2 - 18, 72, 36))
         timer = pygame.font.SysFont('arial', 29, 1).render(f'{time_now:.1f}', 1, (230, 230, 230))
         timer_size = timer.get_size()
         screen.blit(timer, (WINDOW_SIZE[0] * 4 / 5 - timer_size[0] / 2, top_info_size / 2 - timer_size[1] / 2))
@@ -203,7 +228,7 @@ while 1:
     # win condition
     if MS.minecount_const == sum_of_closed_tiles and game_is_going:
         game_is_going = 0
-        clear(cur_tile[0], cur_tile[1], cur_tile[3], cur_tile[2])
+        clear(cur_tile[0], cur_tile[1], cur_tile[2])
 
     for event in pygame.event.get():
         if event.type == QUIT:
